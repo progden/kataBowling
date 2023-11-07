778d81632147dfe140650e0d0cc814fab4c62807
progden <ccdrumer@gmail.com>
2023-11-07 21:38:14 +0800

    Initial commit

---
+++ src/main/java/io/progden/katabowling/KataBowlingApplication.java
@@ -0,0 +1,13 @@
+package io.progden.katabowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBowlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataBowlingApplication.class, args);
+    }
+
+}
